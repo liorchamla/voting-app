550e2215e14c2f1398ad111e3c80edfd7854c418
Lior Chamla <lchamla@gmail.com>
2016-04-27 08:14:57 +0000

    adding polls logic and persistance (todo : dry)

--- server.js
+++ server.js
@@ -12,9 +12,14 @@ var server = http.createServer(router);
 var Flutter = require('flutter');
 var session = require('express-session');
 var mongo = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
 var dbURI = 'mongodb://localhost:27017/polls';
 var RedisStore = require('connect-redis')(session);
 var redisOptions = { host: 'localhost', port: 6379, database: 0, options: {} };
+var async = require("async");
+ // Charge le middleware de sessions
+var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // Using the .html extension instead of
 // having to name the views as *.ejs
@@ -83,8 +88,99 @@ var flutter = new Flutter({
 // Serve the index page
 router.get('/', function(req, res){
     console.log(req.session.user);
-    res.render('index', {
+    var polls = [];
+    
+    // connexion to mongo
+    mongo.connect(dbURI, function(err, db) {
+        if (err) throw err
+        db.collection('polls').find().toArray().then(function(polls){
+            console.log(polls);
+            
+            res.render('index', {
+                req: req,
+                polls: polls
+            });
+        });
+    
+    });
+});
+
+router.get('/polls/new', function(req, res){
+    res.render('polls/new', {
         req: req
+    });
+});
+
+router.post('/polls/create', urlencodedParser, function(req, res){
+    var body = req.body;
+    var answers = [];
+    var possibilities = body.answers.split(',');
+    for(i in possibilities){
+        answers.push({
+            _id: ObjectId(),
+            label: possibilities[i],
+            hits: 0
+        });
+    }
+    var poll = {
+        title: body.title,
+        question: body.question,
+        answers: answers,
+        user_id: req.session.user.id
+    };
+    
+    
+    // connexion to mongo
+    mongo.connect(dbURI, function(err, db) {
+        if (err) throw err
+        db.collection('polls').insertOne(poll, function(err, result){
+            if(err) throw err;
+            console.log(result);
+            res.redirect('/');
+        });
+    });
+});
+
+router.get('/polls/:poll_id/vote/:answer_id', function(req, res){
+    var poll_id = req.params.poll_id;
+    var answer_id = req.params.answer_id;
+    if(req.session.user.hasVoted == undefined){
+        req.session.user.hasVoted = {};
+    } else if(req.session.user.hasVoted[poll_id] != undefined){
+        req.session.flash = "Vous avez déjà voté pour ce sondage !";
+        return res.redirect('/polls/' + poll_id);
+    }
+    
+    mongo.connect(dbURI, function(err, db) {
+        if (err) throw err
+        db.collection('polls').update({"_id": ObjectId(poll_id), "answers._id": ObjectId(answer_id)}, {
+            $inc: { "answers.$.hits" : 1}
+        });
+        req.session.user.hasVoted[poll_id] = true;
+        res.redirect('/polls/' + poll_id);
+    });
+})
+
+router.get('/polls/removeAll', function(req, res){
+    mongo.connect(dbURI, function(err, db) {
+        if (err) throw err
+        db.collection('polls').remove({});
+        res.redirect('/');
+    });
+})
+
+router.get('/polls/:id', function(req, res){
+    var id = req.params.id;
+    console.log(id);
+    mongo.connect(dbURI, function(err, db) {
+        if (err) throw err
+        db.collection('polls').findOne({_id: ObjectId(id)}, function(err, doc){
+            console.log(doc);
+            res.render('polls/show', {
+                req: req,
+                poll: doc
+            })
+        });
     });
 });
 
